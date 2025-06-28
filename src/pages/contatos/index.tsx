109b0ca66a6b2d9bfd0f7e09dfef8bfc3c1740e1
Adenilson Soares <adenilson_denis8@hotmail.com>
2025-06-28 00:25:14 -0300

    Implementação contatos

--- src/pages/contatos/index.tsx
+++ src/pages/contatos/index.tsx
@@ -1,12 +1,17 @@
-import { Footer } from "../../components/footer"
-import { Header } from "../../components/header"
+import { Footer } from "../../components/footer";
+import { Header } from "../../components/header";
+import styles from "./contatos.module.css";
 
-export function Contatos(){
-    return(
-        <div>
-            <Header/>
-            <h1>Contatos</h1>
-            <Footer/>
-        </div>
-    )
+export function Contatos() {
+    return (
+        <main>
+            <Header />
+            <div className={styles.containerContatos}>
+                <p className={styles.contatoItem}>📱 WhatsApp: (11) 91234-5678</p>
+                <p className={styles.contatoItem}>📧 E-mail: contato@exemplo.com</p>
+                <p className={styles.contatoItem}>📷 Instagram: @seudominio</p>
+            </div>
+            <Footer />
+        </main>
+    );
 }
