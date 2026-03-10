c1e3e2ac5c80f1a9bc69db704bac2c58e3d557e6
Adenilson Soares <adenilson_denis8@hotmail.com>
2026-03-09 21:21:10 -0300

    Correção Scroll

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import { Header } from "../components/header";
 import { Footer } from "../components/footer";
 import styles from "./layout.module.css";
+import { ScrollRestoration } from "react-router-dom";
 
 export function Layout() {
     return (
@@ -10,6 +11,7 @@ export function Layout() {
             
             <main className={styles.content}>
                 <Outlet />
+                <ScrollRestoration />
             </main>           
             <Footer />
         </div>
